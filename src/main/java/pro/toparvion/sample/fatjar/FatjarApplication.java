724bd00478aea23ff64c572b1c11fb46762895d6
Toparvion <toparvion@gmx.com>
2020-11-01 21:38:07 +0700

    Initial commit

---
+++ src/main/java/pro/toparvion/sample/fatjar/FatjarApplication.java
@@ -0,0 +1,39 @@
+package pro.toparvion.sample.fatjar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static java.lang.String.join;
+
+@SpringBootApplication
+public class FatjarApplication implements ApplicationRunner {
+  private static final Logger log = LoggerFactory.getLogger(FatjarApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(FatjarApplication.class, args);
+  }
+
+  @Override
+  public void run(ApplicationArguments args) {
+    String[] classPath = System.getProperty("java.class.path").split(System.getProperty("path.separator"));
+    String separatedLines = join("\n", classPath);
+    log.info("'java.class.path' JVM property value ({} entries):\n{}", classPath.length, separatedLines);
+    log.info("'java.protocol.handler.pkgs' JVM property value: {}", System.getProperty("java.protocol.handler.pkgs", "(n/a)"));
+/*
+    //Example using JSR 233 Scripting for Java 6
+    ScriptEngineManager mgr = new ScriptEngineManager();
+    ScriptEngine rbEngine = mgr.getEngineByExtension("rb");
+    try {
+      rbEngine.eval("puts 'Hello World from JRuby!'");
+    } catch (ScriptException ex) {
+      log.error("Ну дела", ex);
+    }
+*/
+
+  }
+
+}
---
+++ src/test/java/pro/toparvion/sample/fatjar/FatjarApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.toparvion.sample.fatjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatjarApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
